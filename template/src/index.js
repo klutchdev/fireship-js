1943419548b17ea09d8b1da4e8b44a31785411c8
Kyle "Klutch" Leary <klutchdevelopment@gmail.com>
2022-02-09 02:47:02 -0500

    Update index.js

--- template/src/index.js
+++ template/src/index.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import { render } from "react-dom";
 import "./index.css";
 import App from "./App";
 
-render(<App />, document.getElementById("root"));
+
+const root = document.getElementById("root");
+
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  root,
+);
